3c293f7f75e30b1f9d7a7d68fc9255f1122305fa
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-10-23 06:07:44 +0000

    ユーザー登録機能を作成

---
+++ work_dic_day10-master/app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+  def new
+  @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+  params.require(:user).permit(:name, :email, :password)
+  end
+end
